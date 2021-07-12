d38c65d4d0be39e0811f571a84370c751a1c4d91
matthieu661 <matthieu.bois@outlook.fr>
2021-07-12 21:30:04 +0200

    reserved page demo

--- frontend-p7-vue-cli-app/src/views/About.vue
+++ frontend-p7-vue-cli-app/src/views/About.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Guide de présentation du Projet 7 " groupomania "</h1>
+    <p><i class="fas fa-wrench"></i> en cours <i class="fas fa-wrench"></i></p>
+    <h2>Page à ne pas prendre en compte</h2> 
   </div>
 </template>
+
+
